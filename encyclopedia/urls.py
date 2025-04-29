036160889e74d145d7a8f61ede634e8bec097987
jlzlt <juliusrackys@gmail.com>
2025-04-29 19:10:42 +0300

    Finished Edit Page feature and Random Page feature

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("edit/<str:title>/", views.edit, name="edit"),
+    path("random/", views.random_entry, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 import markdown2
+import random
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.files.storage import default_storage
 
 from . import util
 
@@ -19,7 +21,7 @@ def entry(request, title):
     entry_html = markdown2.markdown(entry)
 
     return render(request, "encyclopedia/entry.html", {
-        "title": title.capitalize(),
+        "title": title,
         "entry": entry_html
     })
 
@@ -36,10 +38,9 @@ def search(request):
             "title": q.capitalize(),
             "entry": entry_html
         })
-    else:
+    else:        
         # Handle partial matches (substring search)
         matching_entries = [e for e in all_entries if q.lower() in e.lower()]
-        print(matching_entries)
         return render(request, "encyclopedia/search.html", {
             "query": q,
             "results": matching_entries            
@@ -78,8 +79,56 @@ def create(request):
         entry_html = markdown2.markdown(entry)
 
         return render(request, "encyclopedia/entry.html", {
-            "title": title.capitalize(),
+            "title": title,
             "entry": entry_html
         })
 
     return render(request, "encyclopedia/create.html")
+
+def edit(request, title):
+    if request.method == "POST":
+        new_title = request.POST.get('title', '').strip()
+        raw_content = request.POST.get('textbox', '')
+        content = "\n".join(line.rstrip() for line in raw_content.splitlines()).strip()
+
+        if not new_title:
+            return render(request, "encyclopedia/edit.html", {
+                "empty_title": "Title field cannot be empty",
+                "title": title,
+                "content": content   
+            })
+        
+        elif not content:
+            return render(request, "encyclopedia/edit.html", {
+                "content_error": "Content field cannot be empty",
+                "title": new_title,
+                "content": content      
+            })
+        
+        elif util.get_entry(new_title) and new_title != title:
+            return render(request, "encyclopedia/edit.html", {
+                "exists_error": "This title already exists",
+                "title": title,
+                "content": content              
+            })
+        
+        filename = f"entries/{title}.md"
+        default_storage.delete(filename)
+
+        util.save_entry(new_title, content)
+
+        return redirect(f"/{new_title}")
+
+    content = util.get_entry(title)
+    
+    return render(request, "encyclopedia/edit.html", {
+        "title": title,
+        "content": content
+    })
+
+def random_entry(request):
+    entries = util.list_entries()
+
+    random_entry = random.choice(entries)
+
+    return redirect(f"/{random_entry}")
