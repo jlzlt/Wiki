54d22f050f9f1bb8445d96c734bf99781bba86c2
jlzlt <juliusrackys@gmail.com>
2025-04-29 12:15:23 +0300

    Finished search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -22,3 +22,25 @@ def entry(request, title):
         "title": title.capitalize(),
         "entry": entry_html
     })
+
+def search(request):
+    q = request.GET.get('q', '').strip()
+
+    entry = util.get_entry(q)
+    all_entries = util.list_entries()
+
+    if entry is not None:
+        # If search query is available as an entry generate page
+        entry_html = markdown2.markdown(entry)
+        return render(request, "encyclopedia/entry.html", {
+            "title": q.capitalize(),
+            "entry": entry_html
+        })
+    else:
+        # Handle partial matches (substring search)
+        matching_entries = [e for e in all_entries if q.lower() in e.lower()]
+        print(matching_entries)
+        return render(request, "encyclopedia/search.html", {
+            "query": q,
+            "results": matching_entries            
+        })
